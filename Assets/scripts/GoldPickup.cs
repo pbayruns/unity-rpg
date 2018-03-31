299f06ce5163057d09c44c9a723eb3b697c6fa48
Paul Bayruns <bayrunsp9@students.rowan.edu>
2018-03-30 23:21:29 -0400

    added gold system

---
+++ Assets/scripts/GoldPickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldPickup : MonoBehaviour {
+
+    public int value;
+    public MoneyManager theMM;
+
+	// Use this for initialization
+	void Start () {
+        theMM = FindObjectOfType<MoneyManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == "player")
+        {
+            theMM.AddMoney(value);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/scripts/MoneyManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyManager : MonoBehaviour {
+
+    public Text moneyText;
+    public int currentGold;
+
+	// Use this for initialization
+	void Start () {
+        if (PlayerPrefs.HasKey("CurrentMoney"))
+        {
+            currentGold = PlayerPrefs.GetInt("CurrentMoney");
+        }
+        else
+        {
+            currentGold = 0;
+            PlayerPrefs.SetInt("CurrentMoney", 0);
+        }
+
+        moneyText.text = "Gold: " + currentGold;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddMoney(int gold)
+    {
+        currentGold += gold;
+        PlayerPrefs.SetInt("CurrentMoney", currentGold);
+        moneyText.text = "Gold: " + currentGold;
+    }
+}
